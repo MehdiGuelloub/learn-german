1e4f7bde9cde8df1bf70f8e1c4ef04ee80a1d5f8
Mehdi Gloub <mehdi.guelloub@hotmail.fr>
2020-10-15 13:14:58 +0200

    add total mistakes rate

--- app/models/word.rb
+++ app/models/word.rb
@@ -13,6 +13,9 @@ class Word < ApplicationRecord
     scope :never_attempted, -> { where(attempts: 0) }
     scope :search, -> (term) { where("word ILIKE ?", "%#{term}%").or(where("meaning ILIKE ?", "%#{term}%")) }
     scope :mistaken, -> { where.not(attempts: 0).where(consecutive_correct_answers: 0) }
+    scope :total_attempts, -> { sum(:attempts) }
+    scope :total_mistakes, -> { sum(:mistakes) }
+    scope :mistak_by_attempts, -> { sum(:mistakes).to_f / sum(:attempts).to_f * 100 }
 
     def mistakes_rate
         return mistakes.to_f/attempts.to_f * 100 if attempts > 0
