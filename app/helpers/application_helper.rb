56d31b0935453c7d682c30ed5ec3a8c0f7ca7c76
Mehdi Gloub <mehdi.guelloub@hotmail.fr>
2021-01-07 17:22:42 +0100

    minor improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def article_color(word)
+    case
+    when word.article_der? 
+      "badge-danger"
+    when word.article_die? 
+      "badge-success"
+    when word.article_das? 
+      "badge-warning"
+    else
+      ""
+    end
+  end
 end
