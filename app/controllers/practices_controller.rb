3086d83cf03a1836dfa369b4f12cc0d7f047af40
Mehdi Gloub <mehdi.guelloub@hotmail.fr>
2021-02-13 13:14:41 +0100

    add practices calendar

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,5 +1,12 @@
 class PracticesController < ApplicationController
 	def index
-		@practices = Practice.order(date: :DESC)
+		params[:month] ||= Date.today.month
+		params[:year] ||= Date.today.year
+		@current_date = Date.new(params[:year].to_i, params[:month].to_i)
+		@month_days = @current_date.beginning_of_month.step(@current_date.end_of_month)
+		@indexed_practices = Practice
+			.where(date: @current_date.beginning_of_month..@current_date.end_of_month)
+			.order(date: :DESC)
+			.index_by(&:date)
 	end
 end
